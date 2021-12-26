b1dd453125d751a70e097b1e941c377141bdbaa5
GeorgeHenriqueGH <georgehenriquecampos@gmail.com>
2021-12-26 13:46:29 -0300

    DTO for Tecnico

---
+++ backend/helpdesk/src/main/java/com/henrique/helpdesk/dtos/TecnicoDTO.java
@@ -0,0 +1,51 @@
+package com.henrique.helpdesk.dtos;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.henrique.helpdesk.domain.Tecnico;
+import com.henrique.helpdesk.domain.enums.Perfil;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@SuppressWarnings("serial")
+public class TecnicoDTO implements Serializable {
+
+	protected Integer id;
+	protected String nome;
+	protected String cpf;
+	
+	protected String email;
+	protected String senha;
+	protected Set<Integer> perfis = new HashSet<>();
+	
+	@JsonFormat(pattern = "dd/MM/yyyy")
+	protected LocalDate dataCriacao = LocalDate.now();
+
+	public TecnicoDTO() {
+		super();
+	}
+
+	public TecnicoDTO(Tecnico obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
+	}
+	
+	public Set<Perfil> getPerfis() {
+		return perfis.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet());
+	}
+	
+}
