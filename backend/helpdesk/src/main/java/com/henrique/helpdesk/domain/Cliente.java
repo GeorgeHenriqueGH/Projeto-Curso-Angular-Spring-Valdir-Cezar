7dabde90ab4b26f886de73e9de6143d80649b3dc
GeorgeHenriqueGH <georgehenriquecampos@gmail.com>
2022-01-03 15:48:29 -0300

    C.R.U.D cliente

--- backend/helpdesk/src/main/java/com/henrique/helpdesk/domain/Cliente.java
+++ backend/helpdesk/src/main/java/com/henrique/helpdesk/domain/Cliente.java
@@ -1,12 +1,14 @@
 package com.henrique.helpdesk.domain;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.henrique.helpdesk.domain.enums.Perfil;
+import com.henrique.helpdesk.dtos.ClienteDTO;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -31,6 +33,17 @@ public class Cliente extends Pessoa {
 	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
 		addPerfil(Perfil.CLIENTE);
+	}
+	
+	public Cliente(ClienteDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
 	}
 	
 }
